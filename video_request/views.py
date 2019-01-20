1bc70d6faf50f9d162b471667331b510bbffe842
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-01-19 22:55:07 -0500

    videosrequest page fetch data from database

--- video_request/views.py
+++ video_request/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import Video
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'video_request/index.html')
+    videos = Video.objects.order_by('date_added')
+    context = {'videos': videos}
+    return render(request, 'video_request/index.html', context)
 
 
 def vrform(request):
