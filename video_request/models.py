bbf1770f17100f20352f98d8f78ab2bec58aeafe
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-01-19 22:20:17 -0500

    Add view, models, and static files

--- video_request/models.py
+++ video_request/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Video(models.Model):
+    video_title = models.CharField(max_length=20)
+    video_desc = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return 'Name: {}, ID: {}'.format(self.video_title, self.id)
+
--- video_request/views.py
+++ video_request/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return render(request, 'video_request/index.html')
+
+
+def vrform(request):
+    return render(request, 'video_request/vrform.html')
